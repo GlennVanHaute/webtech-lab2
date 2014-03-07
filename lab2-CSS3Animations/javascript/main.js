5dbf5be65a2142339c6307ab8d0e0c0ffd91c735
GlennVanHaute <glennvanhaute@gmail.com>
2014-03-07 20:28:15 +0100

    animations added

---
+++ lab2-CSS3Animations/javascript/main.js
@@ -0,0 +1,9 @@
+         $(document).ready(function(){
+             $("#bol1").click(function () { 
+                 $(".bol1").addClass("animatie1");
+                 $(".bol2").addClass("animatie2");
+                 $(".bol3").addClass("animatie3");
+        });
+     
+
+  }); 
